a7fe148cc1697eacc080d93dccfafefe9d950888
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-07-21 12:16:43 +0200

    Use Caching for Expensive Operations

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -17,17 +17,18 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('posts/{post}', function ($slug)
-{
+Route::get('posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (! file_exists($path))
-    {
+    if (!file_exists($path)) {
         return redirect('/');
     }
 
+    $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+        var_dump('file_get_contents');
+        return $post = file_get_contents($path);
+    });
 
-    $post = file_get_contents($path);
 
     return view('post', [
         'post' => $post
