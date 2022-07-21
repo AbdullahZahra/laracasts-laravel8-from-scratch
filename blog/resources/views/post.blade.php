53f7ff057e38d1229290b2c5e74d4b065126245d
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-07-21 09:26:48 +0200

    Make a Route and Link to it

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque corrupti, magni sint tempore nihil voluptatibus veritatis praesentium quas. Delectus ratione optio tempora! Tenetur nesciunt quos eum sint sapiente ad non?</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+</html>
---
+++ blog/resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque corrupti, magni sint tempore nihil voluptatibus veritatis praesentium quas. Delectus ratione optio tempora! Tenetur nesciunt quos eum sint sapiente ad non?</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque corrupti, magni sint tempore nihil voluptatibus veritatis praesentium quas. Delectus ratione optio tempora! Tenetur nesciunt quos eum sint sapiente ad non?</p>
+    </article>
+    
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque corrupti, magni sint tempore nihil voluptatibus veritatis praesentium quas. Delectus ratione optio tempora! Tenetur nesciunt quos eum sint sapiente ad non?</p>
+    </article>
+</body>
+</html>
