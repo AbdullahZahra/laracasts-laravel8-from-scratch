37f6815e11ef06b3a25180ad5a959fd1422efc96
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-07-28 08:45:59 +0200

    categories

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -11,7 +11,12 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
     public function getRouteKeyName()
     {
